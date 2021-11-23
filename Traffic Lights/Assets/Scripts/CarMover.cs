4773a71ac4e726daa20cf06a1dc10f881e9aeb76
MikhailShevchuk <cp91@mail.ru>
2021-11-23 12:30:46 +0500

    Add car move

--- Traffic Lights/Assets/Scripts/CarMover.cs
+++ Traffic Lights/Assets/Scripts/CarMover.cs
@@ -1,15 +1,41 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class CarMover : MonoBehaviour
 {
+    private float _movingSpeed = 1.0f;
+
+    public float MovingSpeed => _movingSpeed;
+
     private Transform _destinationPoint;
 
     public Transform DestinationPoint => _destinationPoint;
 
+    private Vector3 _startPosition;
+
+    private float _startTime;
+
+    private void Start()
+    {
+        _startPosition = transform.position;
+        _startTime = Time.time;
+    }
+
+    private void Update()
+    {
+        if (_destinationPoint != null)
+        {
+            transform.position = Vector3.Lerp(_startPosition, _destinationPoint.position, Time.time - _startTime * _movingSpeed);
+        }
+    }
+
+
     public void SetDestinationPoint(Transform point)
     {
         _destinationPoint = point;
+    }
+
+    public void SetSpeed(float speed)
+    {
+        _movingSpeed = speed;
     }
 }
