637da6f51fec5b836e0cd0772843dd757ee127c6
MikhailShevchuk <cp91@mail.ru>
2021-11-23 12:14:06 +0500

    Add cars spawner

---
+++ Traffic Lights/Assets/Scripts/CarsSpawner.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarsSpawner : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject _carPrefab;
+
+    [SerializeField]
+    private int _countToSpawn;
+
+    [SerializeField]
+    private float _timeBetweenSpawning = 1.0f;
+
+    public int CountToSpawn => _countToSpawn;
+
+    private void Start()
+    {
+        StartCoroutine("Spawn");
+    }
+
+    public void SetCountToSpawn(int count)
+    {
+        _countToSpawn = count;
+    }
+
+    public IEnumerator Spawn()
+    {
+        for (int i = 0; i < _countToSpawn; i++)
+        {
+            Instantiate(_carPrefab, transform);
+            yield return new WaitForSeconds(_timeBetweenSpawning);
+        }
+    }
+}
